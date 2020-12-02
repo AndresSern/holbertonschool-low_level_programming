d0e58185c2c57e70ce97c39a7e67ec758ab02080
AndresSern <andrecamposer@gmail.com>
2020-12-02 01:34:28 +0000

    Print alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Print alphabet backwards
+* Return: 0 for success
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
