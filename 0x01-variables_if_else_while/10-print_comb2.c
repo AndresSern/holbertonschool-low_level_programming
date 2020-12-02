fd8be5d00aaccbed930593b147de988bb4c9d6b9
AndresSern <andrecamposer@gmail.com>
2020-12-02 02:02:59 +0000

    Print numbers '00' to '99' separeted by a comma

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Print numbers '00' to '99' separeted by a comma
+* Return: 0 for success
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 100; digit++)
+	{
+	putchar((digit / 10) + '0');
+	putchar((digit % 10) + '0');
+		if (digit != 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
