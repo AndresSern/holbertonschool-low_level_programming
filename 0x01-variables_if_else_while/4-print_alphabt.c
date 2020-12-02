ccfac362bb4bf9f402332b69364ba1f45c4e1fb1
AndresSern <andrecamposer@gmail.com>
2020-12-02 01:26:46 +0000

    Alphabet, except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Alphabet between "a" and "z", except "q" and "e" 
+* Return: 0 for success
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if ((letter != 'q') && (letter != 'e'))
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
