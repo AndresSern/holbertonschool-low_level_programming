e6e978e250fc8630bb1b3be8a8e1a7a3653917b6
AndresSern <andrecamposer@gmail.com>
2020-12-02 02:30:47 +0000

    Print numbers from 00 to 99 without repeating

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Print numbers from 00 to 99 without repeating
+* Return: 0 for success
+*/
+int main(void)
+{
+	int digit, first, last, reversed;
+
+	for (digit = 1; digit < 100; digit++)
+	{
+		first = digit / 10;
+		last = digit % 10;
+		reversed = (last * 10) + first;
+		if (digit < reversed)
+		{
+			putchar((digit / 10) + '0');
+			putchar((digit % 10) + '0');
+
+			if (digit != 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
