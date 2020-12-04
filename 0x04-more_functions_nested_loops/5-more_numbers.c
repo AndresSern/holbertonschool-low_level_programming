04056a191958d5104c8c3e2caaef58dbef64958c
AndresSern <andrecamposer@gmail.com>
2020-12-04 03:08:16 +0000

    Variable name changed, and the comment main changed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,24 @@
 #include "holberton.h"
 
 /**
- * more_numbers - print numbers of 0 to 14 with putchar
- *
- *
- *
- *
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  *
  */
 
 void more_numbers(void)
 {
-	int a, b;
+	int replay, count;
 
-	for (a = 0; a <= 9; a++)
+	for (replay = 0; replay <= 9; replay++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (count = 0; count <= 14; count++)
 		{
 
-			if (b >= 10 && b <= 14)
+			if (count >= 10 && count <= 14)
 			{
-				_putchar((b / 10) + '0');
+				_putchar((count / 10) + '0');
 			}
-				_putchar((b % 10) + '0');
+				_putchar((count % 10) + '0');
 		}
 	_putchar('\n');
 	}
