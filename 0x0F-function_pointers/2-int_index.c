766379351b0629727551e0a8969381d1f475263f
AndresSern <andrecamposer@gmail.com>
2020-12-10 07:27:32 +0000

    Betty Style

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
  *
  * @size: This is the length of the array
  * @array: This is input array
- * @cmp: Is a pointer to the function to be used to compare values 
+ * @cmp: Is a pointer to the function to be used to compare values
  *
  * Return: int_index returns the index of the first element for which the cmp
  *         function does not return 0
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,14 +1,14 @@
 #include "3-calc.h"
 /**
  * get_op_func - function that selects the correct function to perform the
- *               operation asked by the user. 
+ *               operation asked by the user.
  *
  * @s: This is the input operator
  *
  * Return: This function returns a pointer to the function that corresponds to
  *         the operator given as a parameter. Example: get_op_func("+")
  *         should return a pointer to the function op_add"")
- *         If s does not match any of the 5 expected operators 
+ *         If s does not match any of the 5 expected operators
  *         (+, -, *, /, %),return NULL)
  *
  */
