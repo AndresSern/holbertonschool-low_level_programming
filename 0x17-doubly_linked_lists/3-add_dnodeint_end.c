e560dca622058625431f0a4ad4b33ced0c813e57
AndresSern <andrecamposer@gmail.com>
2020-12-13 19:54:14 +0000

    Adding a new node in the end of the struct

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Function that adds a new node at the end of a list_t
+ *                    list.
+ *
+ * @head: This is the input double linked list
+ * @n: This is the input number to take the struct
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *last = *head;
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (new_node);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return (*head);
+}
