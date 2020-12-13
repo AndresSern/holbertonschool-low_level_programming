00dc0940dd25de722e1308d9e74143478edc2d35
AndresSern <andrecamposer@gmail.com>
2020-12-13 19:02:15 +0000

    Print each elements of the double list link

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_dlistint -Function that prints all the elements of a double list link
+ *
+ * h: This is the input struct
+ *
+ * Return: Number of the element of the struct
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{ 
+        	printf("%d\n", h->n); 
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include<string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
