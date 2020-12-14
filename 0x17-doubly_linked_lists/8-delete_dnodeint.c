4e85ca21e4e1455e4a9fea698fce80e9930ab8f2
AndresSern <andrecamposer@gmail.com>
2020-12-14 02:42:38 +0000

    Delete node int

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+  * delete_dnodeint_at_index - Function that deletes the node at inde of
+  * a listint_t linked list.
+  *
+  * @head: This is the input list linked
+  * @index: is the index of the node that should be deleted
+  *
+  * Return: 1 if success, -1 if it failed
+  *
+**/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *actual = *head;
+	listint_t *node;
+
+	if (!*head)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(actual);
+		return (1);
+	}
+	while (actual)
+	{
+		if (i  == index - 1)
+		{
+			node = actual->next;
+			actual->next = node->next;
+			free(node);
+			return (1);
+		}
+		actual = actual->next;
+		i++;
+	}
+	return (-1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -30,4 +30,5 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #endif
