d1a06827965a86bc4543cb88f59b13330e76976e
AndresSern <andrecamposer@gmail.com>
2020-12-14 02:20:10 +0000

    Testing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,6 +5,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	/*unsigned int count = 0;*/
 	dlistint_t *new_node, *current_node = *h;
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
@@ -24,18 +25,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		*h = new_node;
-		if(current_node != NULL)
+		if (current_node != NULL)
 			current_node->prev = new_node;
 		return (new_node);
 	}
 	new_node = comprobacion(h, idx, new_node);
-		if (new_node == NULL)
-			return NULL;
+	if (new_node == NULL)
+		return (NULL);
 	return (new_node);
 }
 dlistint_t *comprobacion(dlistint_t **h, int idx, dlistint_t *new_node)
 {
-	int count;
+	int count = 0
 	dlistint_t *current_node = *h;
 
 	for (; count < idx - 1; count++)
@@ -52,7 +53,7 @@ dlistint_t *comprobacion(dlistint_t **h, int idx, dlistint_t *new_node)
 	current_node->next = new_node;
 	current_node = current_node->next;
 	current_node = current_node->next;
-	if(current_node != NULL)
+	if (current_node != NULL)
 		current_node->prev = new_node;
-	return new_node;
+	return (new_node);
 }
