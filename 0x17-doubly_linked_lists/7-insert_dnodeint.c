dcbaaf3564c364ee82069e87e8c3d541baf1b2f3
AndresSern <andrecamposer@gmail.com>
2020-12-14 20:12:39 +0000

    Testing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,51 +12,51 @@
   *         If it is not possible to add the new node at index idx, do not
   *         add the new node and return NULL
 **/
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/*unsigned int count = 0;*/
-	dlistint_t *new_node, *current_node = *h;
-	unsigned int count = 0;
-	new_node = malloc(sizeof(dlistint_t));
+        /*unsigned int count = 0;*/
+        dlistint_t *new_node, *current_node = *h;
+        unsigned int count = 0;
+        new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	if (*h == NULL && idx > 0)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		*h = new_node;
-		if (current_node != NULL)
-			current_node->prev = new_node;
-		return (new_node);
-	}
-	for (; count <= idx ; count++)
-	{
-		current_node = current_node->next;
-		if (current_node == NULL && idx - count > 0)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		if(count == idx - 1)
-		{
-			new_node->prev = current_node;
-			new_node->next = current_node->next;
-			current_node->next = new_node;
-		}
-		if (count == idx)
-			current_node->prev = new_node;
-	}
-	return (new_node);
+        if (!new_node)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        new_node->n = n;
+        new_node->next = NULL;
+        new_node->prev = NULL;
+        if (*h == NULL && idx > 0)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        if (idx == 0)
+        {
+                new_node->next = *h;
+                *h = new_node;
+                if (current_node != NULL)
+                        current_node->prev = new_node;
+                return (new_node);
+        }
+        for (; count <= idx + 1; count++)
+        {
+                /*current_node = current_node->next;*/
+                if (current_node == NULL && idx - count > 0)
+                {
+                        free(new_node);
+                        return (NULL);
+                }
+                if(count == (idx - 1))
+                {
+                        new_node->prev = current_node;
+                        new_node->next = current_node->next;
+                        current_node->next = new_node;
+                }
+                if (count == idx + 1)
+                        current_node->prev = new_node;
+                current_node = current_node->next;
+        }
+        return (new_node);
 }
