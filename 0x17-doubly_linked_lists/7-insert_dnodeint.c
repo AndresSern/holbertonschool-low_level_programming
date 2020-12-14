2783af059f86f4f04fe265e6475a25246dca360e
AndresSern <andrecamposer@gmail.com>
2020-12-14 02:39:31 +0000

    Betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,18 @@
 #include "lists.h"
-dlistint_t *comprobacion(dlistint_t **h, int idx, dlistint_t *new_node);
+dlistint_t *check(dlistint_t **h, int idx, dlistint_t *new_node);
+
+/**
+  * insert_dnodeint_at_index - Function that inserts a new node at a
+  * given position.
+  *
+  * @h: This the input double list linked
+  * @idx: index of the list whete the new node should be added
+  * @n: integer to add to the structure
+  *
+  * Return: The address of the new node, or NULL if it failed
+  *         If it is not possible to add the new node at index idx, do not
+  *         add the new node and return NULL
+**/
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -29,12 +42,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current_node->prev = new_node;
 		return (new_node);
 	}
-	new_node = comprobacion(h, idx, new_node);
+	new_node = check(h, idx, new_node);
 	if (new_node == NULL)
 		return (NULL);
 	return (new_node);
 }
-dlistint_t *comprobacion(dlistint_t **h, int idx, dlistint_t *new_node)
+
+/**
+ * check - Function created to valide if it can be added a node to the double
+ *         list linked, if not return null
+ *
+ * @h: THis is the input double list linked
+ * @idx: Variable Index
+ * @new_node: This is the add new node
+ *
+ * Return: The address of the double list link if not return NULL
+ */
+
+dlistint_t *check(dlistint_t **h, int idx, dlistint_t *new_node)
 {
 	int count = 0;
 	dlistint_t *current_node = *h;
