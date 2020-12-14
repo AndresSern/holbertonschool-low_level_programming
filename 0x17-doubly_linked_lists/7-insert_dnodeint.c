dc6a7547e9588a6e29382a83495d912acdaf25a2
AndresSern <andrecamposer@gmail.com>
2020-12-14 00:55:16 +0000

    Testing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		*h = new_node;
-		current_node->prev = new_node;
+		if(current_node != NULL)
+			current_node->prev = new_node;
 		return (new_node);
 	}
 	for (; count < idx - 1; count++)
