b0a181b228fd6aafd97362a95f5d4be835dff092
AndresSern <andrecamposer@gmail.com>
2020-12-14 00:53:43 +0000

    Testing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,44 +2,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-        unsigned int count = 0;
-        dlistint_t *new_node, *current_node = *h;
-        new_node = malloc(sizeof(dlistint_t));
+	unsigned int count = 0;
+	dlistint_t *new_node, *current_node = *h;
+	new_node = malloc(sizeof(dlistint_t));
 
-        if (!new_node)
-        {
-                free(new_node);
-                return (NULL);
-        }
-        new_node->n = n;
-        new_node->next = NULL;
-        new_node->prev = NULL;
-
-        if (*h == NULL && idx > 0)
-        {
-                free(new_node);
-                return (NULL);
-        }
-        if (idx == 0)
-        {
-                new_node->next = *h;
-                *h = new_node;
-                return (new_node);
-        }
-        for (; count < idx - 1; count++)
-        {
-                current_node = current_node->next;
-                if (current_node == NULL && idx - count > 0)
-                {
-                        free(new_node);
-                        return (NULL);
-                }
-        }
-        new_node->prev = current_node;
-        new_node->next = current_node->next;
-        current_node->next = new_node;
-        current_node = current_node->next;
-        current_node = current_node->next;
-        current_node->prev = new_node;
-        return (new_node);
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*h == NULL && idx > 0)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		current_node->prev = new_node;
+		return (new_node);
+	}
+	for (; count < idx - 1; count++)
+	{
+		current_node = current_node->next;
+		if (current_node == NULL && idx - count > 0)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	new_node->prev = current_node;
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+	current_node = current_node->next;
+	current_node = current_node->next;
+	current_node->prev = new_node;
+	return (new_node);
 }
--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -23,7 +23,7 @@ int main(void)
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 5, 4096);
+    insert_dnodeint_at_index(&head, 7, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
