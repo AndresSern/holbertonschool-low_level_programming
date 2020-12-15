5c6d08f516e0c975fac5dfa7d98939bc6608a7b1
AndresSern <andrecamposer@gmail.com>
2020-12-15 03:18:37 +0000

    Original

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,49 +1,58 @@
-  
 #include "lists.h"
+
 /**
-  * insert_dnodeint_at_index - inserts a new node at a given position
-  * @h: list coming
+  * insert_dnodeint_at_index - Function that inserts a new node at a
+  * given position.
+  *
+  * @h: This the input double list linked
   * @idx: index of the list whete the new node should be added
   * @n: integer to add to the structure
-  * Return: the address of the new node or NULL if it failed
+  *
+  * Return: The address of the new node, or NULL if it failed
+  *         If it is not possible to add the new node at index idx, do not
+  *         add the new node and return NULL
 **/
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *current_node = *h;
+	dlistint_t *new_node, *current_node = *h;
+	unsigned int count = 0;
 
-	if ((!new_node) || (*h == NULL && idx > 0))
-	{
-		free(new_node);
+	if (*h == NULL && idx > 0)
 		return (NULL);
-	}
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		*h = new_node;
 		if (current_node != NULL)
 			current_node->prev = new_node;
-		*h = new_node;
 		return (new_node);
 	}
-	for (; i < idx - 1; i++)
+	for (; count  <= idx; count++)
 	{
-		current_node = current_node->next;
-		if (current_node == NULL && idx - i > 0)
+		if (current_node == NULL)
 		{
 			free(new_node);
 			return (NULL);
 		}
-	}
-	new_node->prev = current_node;
-	new_node->next = current_node->next;
-	current_node->next = new_node;
-	current_node = current_node->next->next;
-	if (current_node != NULL)
-	{
-		current_node->prev = new_node;
+		if (count + 1 == idx)
+		{
+			if (current_node->next != NULL)
+				current_node->next->prev = new_node;
+			new_node->next = current_node->next;
+			current_node->next = new_node;
+			new_node->prev = current_node;
+			return (new_node);
+		}
+		current_node = current_node->next;
 	}
 	return (new_node);
 }
