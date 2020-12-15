f7f07bd100d3858f485484aea8edd8a1647ef121
AndresSern <andrecamposer@gmail.com>
2020-12-15 03:00:33 +0000

    Removing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = NULL;
+	/*new_node->prev = NULL;*/
 	if (idx == 0)
 	{
 		new_node->next = *h;
