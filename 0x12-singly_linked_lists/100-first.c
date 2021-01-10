678a5641e17410c96da84f1bc01c2988b34d2a97
AndresSern <andrecamposer@gmail.com>
2021-01-10 21:47:02 +0000

    Function prints before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * Apply the constructor attribute to startupfun()
+ * so that it is executed before main()
+*/
+	void startupfun(void) __attribute__ ((constructor));
+
+/*
+ * Apply the destructor attribute to cleanupfun()
+ *so that it is executed after main()
+ */
+
+	void cleanupfun(void)__attribute__ ((destructor));
+/**
+ * startupfun - This functions print a text
+ *
+ */
+	void startupfun(void)
+	{
+		printf("You're beat! and yet, you must allow,\t"
+		"\nI bore my house upon my back!\n");
+	}
