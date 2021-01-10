707b4a4e1fa812752477042e380bd600001cb8ce
AndresSern <andrecamposer@gmail.com>
2021-01-10 21:49:41 +0000

    Putting backslash and removing t in the print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -17,6 +17,6 @@
  */
 	void startupfun(void)
 	{
-		printf("You're beat! and yet, you must allow,\t"
-		"\nI bore my house upon my back!\n");
+		printf("You're beat! and yet, you must allow,\
+		\nI bore my house upon my back!\n");
 	}
