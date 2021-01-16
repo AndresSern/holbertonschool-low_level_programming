93b43c7a863c04833714d049d78d1017b3d4a2d0
AndresSern <andrecamposer@gmail.com>
2021-01-16 20:34:54 +0000

    Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,37 +1,50 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_set - Write a function that adds an element to the hash table.
+ *
+ * @ht: Pointer to hash_table_t
+ * @key: Is the key
+ * @value: and value is the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	int returnAdd = 0;
+
 	if (key == NULL || ht == NULL)
-		return 0;
-	index = key_index((unsigned char*)key, ht->size);
-	returnAdd = add_node(&(ht)->array[index], (char *) key,(char *) value);
-	if (returnAdd == 1)
-		return 1;
-	return 0;
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	returAdd = add_node(&(ht)->array[index], (char *)key, (char *)value);
+	if (returAdd == 1)
+		return (1);
+	return (0);
 }
 
 /**
- * add_node - function that adds a new node at the beginning of a list_t list.
+ * add_node - function that adds a new node at the beginning of hash_node_t
  *
- * @head: Pointer to head of the linked list
- * @str: String to assigned to the node
+ * @head: Pointer to head of the hash_node_t
+ * @key: Is the key
+ * @value: and value is the value associated with the key
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
 int add_node(hash_node_t **head, char *key, char *value)
 {
-	hash_node_t *new_node = (hash_node_t *) malloc (sizeof(hash_node_t));
+	hash_node_t *new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	hash_node_t *aux = (*head);
 
 	if (new_node == NULL)
 		return (0);
 
-	while(aux != NULL)
+	while (aux != NULL)
 	{
-		if(strcmp(aux->key, key) == 0)
+		if (strcmp(aux->key, key) == 0)
 		{
 			free(aux->value);
 			aux->value = strdup(value);
