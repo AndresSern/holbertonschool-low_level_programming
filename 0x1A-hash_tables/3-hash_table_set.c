b70c728bf53e89ad376258c2d295eb2cdc638ae3
AndresSern <andrecamposer@gmail.com>
2021-01-16 20:48:03 +0000

    Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	int returnAdd = 0;
+	int returAdd = 0;
 
 	if (key == NULL || ht == NULL)
 		return (0);
