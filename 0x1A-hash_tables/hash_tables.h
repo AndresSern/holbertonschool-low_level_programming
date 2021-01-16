64b7d362de1a434cf6a7fff8de9df834e91ffc5f
AndresSern <andrecamposer@gmail.com>
2021-01-16 22:00:57 +0000

    Inserting the prototype

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,6 +38,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int add_node(hash_node_t **head, char *key, char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 #endif
 
 
