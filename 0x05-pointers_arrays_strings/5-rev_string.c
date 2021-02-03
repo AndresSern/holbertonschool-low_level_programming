52082f3947de0f6ea6e0dd741426f7967ed02b4a
AndresSern <andrecamposer@gmail.com>
2021-02-03 15:33:53 +0000

    Removing a bucle for

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,23 @@
 #include "holberton.h"
 
 /**
- * rev_string - This fuction print a striing reverser
+ * rev_string - This fuction print a string reverser
  *
- *@s: this is the pointer
+ *@s: this is the pointer that point to a string
  */
 
 void rev_string(char *s)
 {
 	int a, len;
 
-	char *begin, *end, x;
+	char *begin, x, *end = s;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
+		end++;
 	}
-
 	len = a;
 	begin = s;
-	end = s;
-	for (a = 0; a < len - 1; a++)
-		end++;
 	for (a = 0; a < len / 2; a++)
 	{
 		x = *end;
