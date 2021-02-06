3505bc6807c041a37d80a5e1624b51a323e14b68
AndresSern <andrecamposer@gmail.com>
2021-02-06 19:58:47 +0000

    Removing a += 1 puttiing in the next line s[a+ 1]

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,8 +19,7 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (a = 0; str[a] != '\0'; a++)
-	{
-	}
+		;
 	s = (char *)  malloc((a + 1) * sizeof(char));
 
 	if (s == NULL)
@@ -29,7 +28,6 @@ char *_strdup(char *str)
 	for (b = 0; b < a ; b++)
 		s[b] = str[b];
 
-	a += 1;
-	s[a] = '\0';
+	s[a + 1] = '\0';
 	return (s);
 }
