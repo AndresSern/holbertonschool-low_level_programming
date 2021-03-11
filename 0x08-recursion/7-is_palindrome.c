cba902be39f6822cb738f6d58476ac6fc65f00b1
AndresSern <andrecamposern@gmail.com>
2021-03-11 15:31:25 +0000

    Adding function

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,31 +1,52 @@
 #include "holberton.h"
 
-int check_is_palindrome(char *s, int minimum, int high)
+#include "holberton.h"
+/**
+  * _lenght - checks the length of a string
+  * @s: is the string
+  * Return: return the leng of the string
+**/
+int _lenght(char *s)
 {
-	if (minimum == high)
-		return (1);
-	if (s[minimum] != s[high])
+	if (*s == '\0')
 		return (0);
-	if (minimum < high + 1)
-		return (check_is_palindrome(s, minimum + 1, high - 1));
-	return (1);
+	return (1 + _lenght(s + 1));
 }
 
 /**
- * is_palindrome
- *
- * @s: Pointer that point to a string
- *
- * Return: If is 1 is a palindrome, If is 0 not is a palindrome
- */
-int is_palindrome(char *s)
+ * checkp - checks if the string is palindrome
+ * @i: is the index
+ * @lg: is the length of the string
+ * @s: is the string
+ * Return: 1 if is polindrome or 0 if not
+**/
+int checkp(int i, int lg, char *s)
 {
-	int len = 0;
-
-	for (len = 0; s[len] != '\0'; len++)
-		;
-	if (len == 0)
-		return (1);
-	return (check_is_palindrome(s, 0, len - 1));
+	if (lg > 0)
+	{
+		if (s[i] == s[lg])
+		{
+			return (checkp(i + 1, lg - 1, s));
+		}
+		else if (s[i] != s[lg])
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (1);
 }
 
+
+/**
+  * is_palindrome - Checks if a string is a palindrome
+  * @s: is the string
+  * Return: return 1 if the string is a palindrome or 0 otherwise
+**/
+int is_palindrome(char *s)
+{
+	return (checkp(0, _lenght(s) - 1, s));
+}
