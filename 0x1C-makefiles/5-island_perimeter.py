f336f04e5261457c84294a5e8a986141798c3736
AndresSern <andrecamposer@gmail.com>
2021-04-12 13:04:03 -0500

    Fixing documentation with comments and fixing pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Thechincal interview preparation
+This module that returns the perimeter of the island describ in grid
+"""
+
 
 def island_perimeter(grid):
+    """ THis functions returns the
+        perimeter of the island described in grid """
     len_verti = len(grid)
     len_Hori = len(grid[0])
     perimeter = 0
@@ -8,9 +15,8 @@ def island_perimeter(grid):
         for j, col in enumerate(row):
             if grid[i][j] == 1:
                 perimeter = perimeter + 4
-                if i > 0 and grid [i-1][j] == 1:
+                if i > 0 and grid[i-1][j] == 1:
                     perimeter = perimeter - 2
-                if j > 0 and grid [i][j-1] == 1:
-                    perimeter = perimeter  -2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter = perimeter - 2
     return perimeter
-
