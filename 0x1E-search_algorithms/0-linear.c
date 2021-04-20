a4535c7f0415a0570823560b286a1343c0161211
AndresSern <andrecamposern@gmail.com>
2021-04-20 17:20:00 +0000

    Fixing betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,6 +16,7 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
 #ifndef HEADER_FILE
-#define HEADER_FILE 
+#define HEADER_FILE
 #include<string.h>
 #include <stdlib.h>
 #include <stdarg.h>
