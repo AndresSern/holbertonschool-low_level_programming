df179e987ca4a8dff3b773b241ff871e780b3581
AndresSern <andrecamposern@gmail.com>
2021-04-20 23:21:12 +0000

    Fixing betty style

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -20,45 +20,68 @@ void printArray(int *array, size_t mid, size_t high);
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL)
-		return -1;
+		return (-1);
 	printArray(array, 0, size - 1);
 
-	return recursion(array, 0, size - 1, value);
+	return (recursion(array, 0, size - 1, value));
 }
 
+/**
+ * recursion - Function that search a index with an array
+ *
+ * @array:  is a pointer to the first element of the array to search in
+ * @low:   is the number lower of index in the array
+ * @high:  Is the number of index bigger in the array
+ *
+ * @value:  is the value to search for
+ *
+ *
+ * Return:  Must return the index where value is located
+ *          If value is not present in array or if array is NULL,
+ *          your function must return -1
+ */
 int recursion(int *array, size_t low, size_t high, int value)
 {
 	size_t mid = 0;
+
 	if (high >= low)
 	{
 		mid =  (low + high) / 2;
 		printArray(array, mid, high);
 		if (array[mid] ==  value)
-			return mid;
+			return (mid);
 
 		if (array[mid] < value)
-			return recursion(array, mid + 1, high, value );
+			return (recursion(array, mid + 1, high, value));
 
-		return recursion(array, low, mid - 1, value );
+		return (recursion(array, low, mid - 1, value));
 	}
 	return (-1);
 }
 
+/**
+ * printArray - function that print an array
+ *
+ * @array:  is a pointer to the first element of the array to search in
+ * @mid:   is the number lower of index in the array
+ * @high:  Is the number of index bigger in the array
+ *
+ */
 void printArray(int *array, size_t mid, size_t high)
 {
 	size_t i = 0;
 
 	for (i = mid; i < high; i++)
-	{	
-		if (i == (mid ))
+	{
+		if (i == (mid))
 			printf("Searching in array: ");
 
-		printf("%d",array[i]);
+		printf("%d", array[i]);
 
-		if (i < (high - 1))	
+		if (i < (high - 1))
 			printf(", ");
 
-		if (i == (high - 1 ))
+		if (i == (high - 1))
 			printf("\n");
 	}
 }
