6234a88bd334f58063677d842d787f76966d481d
AndresSern <andrecamposern@gmail.com>
2021-04-21 22:38:27 +0000

    Adding the file 107

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+#include <math.h>
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t end = sqrt(size), start, i = 1;
+	listint_t *temp = list;
+	if (list == NULL || size == 0)
+		return NULL;
+	while(list != NULL && list->next != NULL)
+	{
+		printf("%d-----%li\n",  list->n, i);
+		printf("%p-----%p\n",  (void*)(list), (void *)(list)->(&(next[2])));
+
+		list = &(list)->next[0];
+		i = i * 2;
+		
+	}
+		return list;
+	(void)temp;
+	(void)size;
+	(void)end;
+	(void)i;
+	(void)start;
+	(void)value;
+}
