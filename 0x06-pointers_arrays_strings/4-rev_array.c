7365ddc4fb9f444996ec6386d5841224b66afa06
AndresSern <andrecamposer@gmail.com>
2021-07-09 12:55:46 -0500

    Putting the variable 'start, end' inside the loop

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int start, end, i, last;
+	int i, last;
 
 	last = n - 1;
 	for (i = 0; i < n / 2; i++)
 	{
+		int start, end;
 		start = a[i];
 		end = a[last];
 		a[i] = end;
