1dcbc3e47e413d305b733f54b1c8a958fc257604
AndresSern <andrecamposer@gmail.com>
2021-07-09 15:38:05 -0500

    comment the variable end

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -3,13 +3,14 @@
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t end = sqrt(size), start, i = 1;
+	// size_t end = sqrt(size), 
+	size_t start, i = 1;
 	listint_t *temp = list;
 	if (list == NULL || size == 0)
 		return NULL;
 	while(list != NULL && list->next != NULL)
 	{
-		printf("%d-----%li\n",  list->n, i);
+		printf("%d-----%zu\n",  list->n, i);
 		printf("%p-----%p\n",  (void*)(list), (void *)(list)->(&(next[2])));
 
 		list = &(list)->next[0];
@@ -19,7 +20,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		return list;
 	(void)temp;
 	(void)size;
-	(void)end;
+	// (void)end;
 	(void)i;
 	(void)start;
 	(void)value;
