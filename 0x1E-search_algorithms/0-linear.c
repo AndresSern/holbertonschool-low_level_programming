020f8529cea94b75655bbfc63a67a3fb5ff38c1a
AndresSern <andrecamposer@gmail.com>
2021-07-09 15:27:11 -0500

    Chagin format %lu to %zu

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%li] = [%d]\n", i, array[i]);
+		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
 		if (value == array[i])
 			return (i);
 	}
