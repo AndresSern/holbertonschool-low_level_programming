aa270a2a2e95a4511a6c437d222e647e63bb2df5
AndresSern <andrecamposer@gmail.com>
2021-07-09 12:40:11 -0500

    Redundant assignment of n to start the loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,10 @@
 void print_to_98(int n)
 {
 	if (n <= 98)
-		for (n = n; n <= 97; n++)
+		for (; n <= 97; n++)
 			printf("%d, ", n);
 	else
-		for (n = n; n > 98; n--)
+		for (; n > 98; n--)
 			printf("%d, ", n);
 	printf("98\n");
 }
