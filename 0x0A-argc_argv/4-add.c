aa8f4bd4b9d002d0e8c19ee0e945976ec234e0bf
AndresSern <andrecamposer@gmail.com>
2021-07-09 13:07:52 -0500

    Putting the variable 'b, and char' inside the loop

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,20 +13,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0, i, b;
-	char *str;
+	int sum = 0, i;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
+			int b;
+			char *str;
+
 			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
 				if (str[b] < 48 || str[b] > 57)
 				{
-				printf("Error\n");
-				return (1);
+					printf("Error\n");
+					return (1);
 				}
 			}
 		}
