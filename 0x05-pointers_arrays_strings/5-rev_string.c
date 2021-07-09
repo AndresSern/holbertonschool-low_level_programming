98466a3636b37ae5146a8bd34c354662db65b8b2
AndresSern <andrecamposer@gmail.com>
2021-07-09 12:52:56 -0500

    Putting the variable 'x' inside the second the loop

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 {
 	int a, len;
 
-	char *begin, x, *end = s;
+	char *begin, *end = s;
 
 	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
@@ -20,6 +20,7 @@ void rev_string(char *s)
 	begin = s;
 	for (a = 0; a < len / 2; a++)
 	{
+		char x;
 		x = *end;
 		*end = *begin;
 		*begin = x;
